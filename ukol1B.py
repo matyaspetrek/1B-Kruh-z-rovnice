7634487d6ca0ff3b0d02f571ecf8dba269d2c360
matyaspetrek <m_petrek@utb.cz>
2026-02-26 10:34:25 +0100

    Update ukol1B.py

--- ukol1B.py
+++ ukol1B.py
@@ -10,6 +10,16 @@ x = 117
 y = 22
 z = -5
 rHead = 11
+stredX = 247
+stredY = -7
+stredZ = -53
 dType.SetPTPCmd(api, 2, x, y, z, rHead, 1)
-
-
+dType.SetPTPCmd(api, 2, stredX, stredY, stredZ, rHead, 1)
+dType.SetPTPCmd(api, 2, stredX, stredY, stredZ+30, rHead, 1)
+for uhelStup in range(360):
+	uhelRad = math.radians(uhelStup)
+	velikost = 15
+	X=stredX + velikost * math.cos(uhelRad)
+	Y=stredY + velikost * math.sin(uhelRad)
+	dType.SetPTPCmd(api, 2, X, Y, stredZ, rHead, 1)
+dType.SetPTPCmd(api, 2, x, y, z, rHead, 1)
