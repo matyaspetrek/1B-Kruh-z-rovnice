cfd7447a06235c5afc5653603f280357ce7d3309
matyaspetrek <m_petrek@utb.cz>
2026-02-26 10:33:09 +0100

    Add files via upload

---
+++ ukol1B.py
@@ -0,0 +1,15 @@
+#Magician
+import math
+
+
+dType.SetPTPJointParams(api,200,200,200,200,200,200,200,200,0)
+dType.SetPTPCoordinateParams(api,200,200,200,200,0)
+dType.SetPTPJumpParams(api, 10, 200,0)
+dType.SetPTPCommonParams(api, 100, 100,0)
+x = 117
+y = 22
+z = -5
+rHead = 11
+dType.SetPTPCmd(api, 2, x, y, z, rHead, 1)
+
+
